5b286809bb242602c26ed2aae5f2c148356ae268
ckhan <chungkyun.han@gmail.com>
2018-01-16 16:02:03 +0800

    temp

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-for i in {1..11}; do
-    python -c "from handling_taxiData import run; run('09$i')" &
+for i in 090{1..9} 09{10..11}; do
+    python -c "from handling_taxiData import run; run('$i')" &
 done
